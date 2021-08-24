39d9ff963fea98192e1cff9f3aab1aa4378a7e26
new name <new email>
2021-08-24 21:32:13 +0600

    second

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -5,33 +5,32 @@ import CommentsReal from "./CommentsReal";
 const Comments = () => {
   const { id } = useParams();
   const [post, setPost] = useState([]);
-  const [comments, setComments] = useState([])
+  const [comments, setComments] = useState([]);
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => res.json())
       .then((data) => setPost(data))
       .catch((err) => console.error(err));
-  }, []);
+  }, [id]);
 
-  useEffect(() =>{
-      fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-      .then(res => res.json())
-      .then(data => setComments(data))
-  }, [])
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, [id]);
 
-  console.log(comments)
-
+  console.log(comments);
 
   const { title, body } = post;
-  const history = useHistory()
-  
+  const history = useHistory();
+
   return (
     <div>
       <h1>{title}</h1>
       <p>{body}</p>
-      {
-          comments.map(comment => <CommentsReal comment={comment}></CommentsReal>)
-      }
+      {comments.map((comment) => (
+        <CommentsReal comment={comment}></CommentsReal>
+      ))}
       <button onClick={history.goBack}>Go Back</button>
     </div>
   );
